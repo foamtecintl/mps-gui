7d61ac4dee05e530fbdacb5b9e955077ebc831fe
foamtecintl <apichate@foamtecintl.com>
2018-07-19 14:44:04 +0700

    add project

---
+++ main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow, dialog} = require('electron')
+const url = require('url')
+const path = require('path')
+
+let mainWindow
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({width: 1150, height: 700})
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, '/login.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+  mainWindow.setResizable(false)
+  mainWindow.setFullScreenable(false)
+  mainWindow.on('close', function(event) {
+    let choice = dialog.showMessageBox(this, {
+      type: 'question',
+      buttons: ['Yes', 'No'],
+      title: 'Confirm',
+      message: 'Are you sure you want to quit?'
+    })
+    if(choice == 1){
+      event.preventDefault()
+    }
+  })
+}
+
+app.on('ready', createMainWindow)
