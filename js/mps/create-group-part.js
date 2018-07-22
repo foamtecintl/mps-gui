43ca5f4a9176058a31ebc8283b5898b8eb1ae808
foamtecintl <apichate@foamtecintl.com>
2018-07-22 22:42:16 +0700

    delete js folder and create page create-group.html

--- js/mps/create-group-part.js
+++
@@ -1,8 +0,0 @@
-var create_group_part_panel = {
-  id: 'create-group-part-panel',
-  title: 'Create Group/Part',
-  layout: 'column',
-  bodyStyle: 'padding:5px',
-  defaults: {bodyStyle:'padding:15px'},
-  items: []
-};
--- js/mps/dashborad.js
+++
@@ -1,8 +0,0 @@
-var dashborad_panel = {
-  id: 'dashborad-panel',
-  title: 'Dashborad',
-  layout: 'column',
-  bodyStyle: 'padding:5px',
-  defaults: {bodyStyle:'padding:15px'},
-  items: []
-};
